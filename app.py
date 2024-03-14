3523c3e473e7dcf469035ee6169faadec6df5a6b
Zer0c4ll <zar0c4ll@proton.me>
2024-03-14 15:02:21 -0300

    initial init

---
+++ app.py
@@ -0,0 +1,103 @@
+import imaplib
+import email
+from dotenv import load_dotenv
+import os
+import pdfkit
+from pypdf import PdfMerger
+from bs4 import BeautifulSoup
+# Carrega as variáveis de ambiente do arquivo .env
+load_dotenv()
+merger = PdfMerger()
+def export_to_pdf(subject, body, ident, files):
+    try:
+        path = 'FOI.pdf'
+        print("Iniciando exportação do PDF...")
+        # # Cria um arquivo PDF com o título do email como nome do arquivo
+        config = pdfkit.configuration(wkhtmltopdf='/usr/local/bin/wkhtmltopdf')
+        print(files)
+
+        # if os.path.exists(path):
+        #     pdfkit.from_string(str(body), 'FOI.pdf', append=True, configuration = config)
+        # elif os.path.exists(path=False):
+        #     pdfkit.from_string(str(body), 'FOI.pdf', configuration = config)
+        pdfkit.from_string(str(body), f'separados/email_{ident}.pdf')
+
+        # if os.path.exists('Final.pdf'):
+        # else:
+        #     pdfkit.from_string('<html><h1>NEW</h1></html>', 'Final.pdf')
+        
+        # config = pdfkit.configuration(wkhtmltopdf='/usr/local/bin/wkhtmltopdf')
+        # pdfkit.from_string(str(body), output_path = 'meu_arquivo.pdf', configuration = config)
+        
+        # print(f"PDF exportado para: {filename}")
+    except Exception as e:
+        print(f"Erro ao exportar PDF: {e}")
+
+
+def fetch_and_export_emails(username, password):
+    try:
+        # Conecta-se ao servidor IMAP do Outlook
+        mail = imaplib.IMAP4_SSL('outlook.office365.com')
+
+        # Loga no servidor IMAP
+        print("Tentando logar...")
+        mail.login(username, password)
+        print("Login bem-sucedido!")
+
+        # Seleciona a caixa de entrada
+        mail.select('inbox')
+
+        # Procura por todos os emails na caixa de entrada
+        result, data = mail.search(None, 'ALL')
+        # if result == 'OK':
+        print("E-mails encontrados. Iniciando processamento...")
+        # Itera sobre os IDs dos emails
+        files = []
+        ident = 1
+        for num in data[0].split():
+            print("Processando email...")
+            # Busca as informações do email pelo ID
+            result, data = mail.fetch(num, '(RFC822)')
+            raw_email = data[0][1]
+            
+            # Decodifica o email
+            msg = email.message_from_bytes(raw_email)
+            
+            # Exibe o remetente e o assunto do email
+            print('From:', msg['From'])
+            print('To: ', msg['To'])
+            print('Subject:', msg['Subject'])
+            
+            # Verifica se há texto sem formatação disponível
+            for part in msg.walk():
+                if part.get_content_type() == 'text/html':
+                    if msg['Subject']:
+                        bodyNEW = part.get_payload(decode=True).decode('ISO-8859-1')
+                        body = BeautifulSoup(bodyNEW)
+                        # Exporta o email para PDF
+                        files.append(f'separados/email_{ident}.pdf')
+                        export_to_pdf(msg['Subject'], body, ident, files)
+                        # ident = ident+1
+                        # break  # Parar após encontrar o corpo de texto sem formatação
+                        ident = ident+1
+                    else:
+                        print('Nenhum email com o subject especificado foi encontrado')
+                else:
+                    print("Nenhum email encontrado na caixa de entrada.")
+        for pdf in files:
+            merger.append(pdf)
+        merger.write('unificados/Final.pdf')
+        merger.close()
+        # Fecha a conexão com o servidor IMAP
+        mail.logout()
+    except Exception as e:
+        print(f"Erro durante a busca e exportação de emails: {e}")
+
+if __name__ == "__main__":
+    # Recupera as credenciais de e-mail e senha do Outlook do arquivo .env
+    email_username = os.getenv("EMAIL")
+    email_password = os.getenv("PASS")
+
+    # Chama a função para buscar e exibir os emails
+    fetch_and_export_emails(email_username, email_password)
+    
